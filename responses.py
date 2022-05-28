02e7519b24ab105184562208596c1f8789b05615
PatrykBochenek <106195747+PatrykBochenek@users.noreply.github.com>
2022-05-28 02:50:58 +0100

    Update responses.py

--- responses.py
+++ responses.py
@@ -2,7 +2,7 @@ from urllib import response
 import openai
 import constants as keys
 
-def sample_responses(input_text):
+def ai_responses(input_text):
     user_message = str(input_text).lower()
     prompt_addition = "ENTER YOUR PROMPT HERE" + user_message
     openai.api_key = keys.OPEN_AI_KEY
